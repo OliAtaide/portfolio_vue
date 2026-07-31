03ef828a7f2cba528fd8cc0575d0b65e5643b2de
Filipe de Oliveira Ataíde <40429958+OliAtaide@users.noreply.github.com>
2026-07-31 10:58:27 -0300

    Adding Vercel Analytics

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import FooterBar from "./components/FooterBar.vue";
 import NavBar from "./components/NavBar.vue";
 import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { Analytics } from "@vercel/analytics/vue"
 </script>
 
 <template>
   <SpeedInsights />
+  <Analytics />
   <NavBar />
   <div class="main">
     <router-view />
